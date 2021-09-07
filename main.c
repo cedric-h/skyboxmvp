0877b35a796bae526c7d126146dd48b4f6bb0456
cedric-h <cedhut02@gmail.com>
2021-09-07 03:25:50 -0400

    Remove bloom, rotate camera with WASD

--- main.c
+++ main.c
@@ -30,78 +30,22 @@
 
 static struct {
   float rx, ry;
-  sg_pipeline pip;
-  sg_buffer ibuf, vbuf, quad_vbuf;
   struct {
-    sg_image color_img, bright_img, depth_img;
+    sg_buffer ibuf, vbuf;
     sg_pass_action pass_action;
-    sg_pass pass;
     sg_pipeline pip;
-  } offscreen;
-  struct {
-    sg_image imgs[2];
-    sg_pass passes[2];
-    sg_pipeline pip;
-  } blur;
+  } mesh;
 } state;
 
-/* must be called on resize */
-void resize_framebuffers(void) {
-  /* destroy previous resource (can be called for invalid id) */
-  sg_destroy_pass(state.offscreen.pass);
-  sg_destroy_image(state.offscreen.color_img);
-  sg_destroy_image(state.offscreen.bright_img);
-  sg_destroy_image(state.offscreen.depth_img);
-  for (int i = 0; i < 2; i++) {
-    sg_destroy_image(state.blur.imgs[i]);
-    sg_destroy_pass(state.blur.passes[i]);
-  }
-
-  /* a render pass with one color- and one depth-attachment image */
-  sg_image_desc img_desc = {
-    .render_target = true,
-    .width = sapp_widthf(),
-    .height = sapp_heightf(),
-    .pixel_format = SG_PIXELFORMAT_RGBA8,
-    .min_filter = SG_FILTER_LINEAR,
-    .mag_filter = SG_FILTER_LINEAR,
-    .wrap_u = SG_WRAP_CLAMP_TO_EDGE,
-    .wrap_v = SG_WRAP_CLAMP_TO_EDGE,
-    .sample_count = OFFSCREEN_SAMPLE_COUNT,
-    .label = "color-image"
-  };
-  state.offscreen.color_img = sg_make_image(&img_desc);
-  state.offscreen.bright_img = sg_make_image(&img_desc);
-
-  for (int i = 0; i < 2; i++)
-    state.blur.imgs[i] = sg_make_image(&img_desc);
-  for (int i = 0; i < 2; i++)
-    state.blur.passes[i] = sg_make_pass(&(sg_pass_desc) {
-      .color_attachments[0].image = state.blur.imgs[i]
-    });
-
-  img_desc.pixel_format = SG_PIXELFORMAT_DEPTH;
-  img_desc.label = "depth-image";
-  sg_image depth_img = state.offscreen.depth_img = sg_make_image(&img_desc);
-  state.offscreen.pass = sg_make_pass(&(sg_pass_desc){
-    .color_attachments[0].image = state.offscreen.color_img,
-    .color_attachments[1].image = state.offscreen.bright_img,
-    .depth_stencil_attachment.image = state.offscreen.depth_img,
-    .label = "offscreen-pass"
-  });
-}
-
 /* can be called once on initialization */
-void offscreen_init(void) {
-  resize_framebuffers();
-
-  /* offscreen pass action */
-  state.offscreen.pass_action = (sg_pass_action) {
+void mesh_init(void) {
+  /* mesh pass action */
+  state.mesh.pass_action = (sg_pass_action) {
     .colors[0] = { .action = SG_ACTION_CLEAR, .value = { 0.25f, 0.25f, 0.25f, 1.0f } },
     .colors[1] = { .action = SG_ACTION_CLEAR, .value = { 0.00f, 0.00f, 0.00f, 0.0f } },
   };
 
-  state.offscreen.pip = sg_make_pipeline(&(sg_pipeline_desc) {
+  state.mesh.pip = sg_make_pipeline(&(sg_pipeline_desc) {
     .layout = {
       .attrs = {
         [ATTR_mesh_vs_position].format = SG_VERTEXFORMAT_FLOAT3,
@@ -111,22 +55,10 @@ void offscreen_init(void) {
     .shader = sg_make_shader(mesh_shader_desc(sg_query_backend())),
     .index_type = SG_INDEXTYPE_UINT16,
     .cull_mode = SG_CULLMODE_FRONT,
-    .color_count = 2,
     .depth = {
-      .pixel_format = SG_PIXELFORMAT_DEPTH,
       .write_enabled = true,
       .compare = SG_COMPAREFUNC_LESS_EQUAL,
     },
-  });
-
-  state.blur.pip = sg_make_pipeline(&(sg_pipeline_desc){
-    .layout = {
-      .attrs[ATTR_fsq_vs_pos].format = SG_VERTEXFORMAT_FLOAT2
-    },
-    .shader = sg_make_shader(blur_shader_desc(sg_query_backend())),
-    .primitive_type = SG_PRIMITIVETYPE_TRIANGLE_STRIP,
-    .depth.pixel_format = SG_PIXELFORMAT_NONE,
-    .label = "blur pipeline"
   });
 }
 
@@ -167,7 +99,7 @@ void init(void) {
     1.0,  1.0,  1.0,   1.0, 0.0, 0.5, 1.0,
     1.0,  1.0, -1.0,   1.0, 0.0, 0.5, 1.0
   };
-  state.vbuf = sg_make_buffer(&(sg_buffer_desc){
+  state.mesh.vbuf = sg_make_buffer(&(sg_buffer_desc){
     .data = SG_RANGE(vertices),
     .label = "cube-vertices"
   });
@@ -181,29 +113,13 @@ void init(void) {
     16, 17, 18,  16, 18, 19,
     22, 21, 20,  23, 22, 20
   };
-  state.ibuf = sg_make_buffer(&(sg_buffer_desc){
+  state.mesh.ibuf = sg_make_buffer(&(sg_buffer_desc){
     .type = SG_BUFFERTYPE_INDEXBUFFER,
     .data = SG_RANGE(indices),
     .label = "cube-indices"
   });
 
-  /* a vertex buffer to render a fullscreen rectangle */
-  state.quad_vbuf = sg_make_buffer(&(sg_buffer_desc){
-    .data = SG_RANGE(((float[]) { 0.0f, 0.0f,  1.0f, 0.0f,  0.0f, 1.0f,  1.0f, 1.0f })),
-    .label = "quad vertices"
-  });
-
-  offscreen_init();
-
-  /* create pipeline object */
-  state.pip = sg_make_pipeline(&(sg_pipeline_desc){
-    .layout = {
-      .attrs[ATTR_fsq_vs_pos].format = SG_VERTEXFORMAT_FLOAT2
-    },
-    .shader = sg_make_shader(fsq_shader_desc(sg_query_backend())),
-    .primitive_type = SG_PRIMITIVETYPE_TRIANGLE_STRIP,
-    .label = "fullscreen quad pipeline"
-  });
+  mesh_init();
 }
 
 static void event(const sapp_event *ev) {
@@ -211,9 +127,14 @@ static void event(const sapp_event *ev) {
     case (SAPP_EVENTTYPE_KEY_DOWN): {
       if (ev->key_code == SAPP_KEYCODE_ESCAPE)
         sapp_request_quit();
-    } break;
-    case (SAPP_EVENTTYPE_RESIZED): {
-      resize_framebuffers();
+      if (ev->key_code == SAPP_KEYCODE_W)
+        state.rx += 0.03f;
+      if (ev->key_code == SAPP_KEYCODE_S)
+        state.rx -= 0.03f;
+      if (ev->key_code == SAPP_KEYCODE_A)
+        state.ry += 0.03f;
+      if (ev->key_code == SAPP_KEYCODE_D)
+        state.ry -= 0.03f;
     } break;
   }
 }
@@ -224,58 +145,26 @@ void frame(void) {
   const float w = sapp_widthf();
   const float h = sapp_heightf();
   Mat4 proj = perspective4x4(1.047f, w/h, 0.01f, 10.0f);
-  Mat4 view = look_at4x4(vec3(0.0f, 1.5f, 6.0f), vec3_f(0.0f), vec3_y);
+  Vec3 eye = mul3_f(vec3(
+    cosf(state.rx)*sinf(state.ry),
+    sinf(state.rx),
+    cosf(state.rx)*cosf(state.ry)
+  ), 6.0f);
+  Mat4 view = look_at4x4(eye, vec3_f(0.0f), vec3_y);
   Mat4 view_proj = mul4x4(proj, view);
 
-  state.rx += 0.01f; state.ry += 0.02f;
-  Mat4 rxm = rotate4x4(vec3_x, state.rx);
-  Mat4 rym = rotate4x4(vec3_y, state.ry);
-  Mat4 model = mul4x4(rxm, rym);
-  vs_params.mvp = mul4x4(view_proj, model);
+  vs_params.mvp = view_proj;
 
-  sg_begin_pass(state.offscreen.pass, &state.offscreen.pass_action);
-  sg_apply_pipeline(state.offscreen.pip);
+  sg_begin_default_pass(&state.mesh.pass_action, (int)w, (int)h);
+  sg_apply_pipeline(state.mesh.pip);
   sg_apply_bindings(&(sg_bindings) {
-    .vertex_buffers[0] = state.vbuf,
-    .index_buffer = state.ibuf
+    .vertex_buffers[0] = state.mesh.vbuf,
+    .index_buffer = state.mesh.ibuf
   });
   sg_apply_uniforms(SG_SHADERSTAGE_VS, SLOT_mesh_vs_params, &SG_RANGE(vs_params));
   sg_draw(0, 36, 1);
   sg_end_pass();
 
-  int horizontal = 1;
-  for (int i = 0; i < 10; i++) {
-    sg_begin_pass(state.blur.passes[horizontal], &(sg_pass_action) {
-      .colors[0] = { .action = SG_ACTION_LOAD }
-    });
-    sg_apply_pipeline(state.blur.pip);
-    sg_apply_bindings(&(sg_bindings) {
-      .vertex_buffers[0] = state.quad_vbuf,
-      .fs_images = {
-        [SLOT_tex] = i ? state.blur.imgs[!horizontal] : state.offscreen.bright_img
-      }
-    });
-    blur_fs_params_t fs_params = { .hori = vec2(horizontal, !horizontal) };
-    sg_apply_uniforms(SG_SHADERSTAGE_FS, SLOT_blur_fs_params, &SG_RANGE(fs_params));
-    sg_draw(0, 4, 1);
-    sg_end_pass();
-    horizontal = !horizontal;
-  }
-
-  sg_pass_action pass_action = {
-    .colors[0] = { .action = SG_ACTION_CLEAR, .value = { 0.0f, 0.0f, 0.0f, 1.0f } }
-  };
-  sg_begin_default_pass(&pass_action, (int)w, (int)h);
-  sg_apply_pipeline(state.pip);
-  sg_apply_bindings(&(sg_bindings) {
-    .vertex_buffers[0] = state.quad_vbuf,
-    .fs_images = {
-      [SLOT_tex] = state.offscreen.color_img,
-      [SLOT_bloomed] = state.blur.imgs[!horizontal]
-    }
-  });
-  sg_draw(0, 4, 1);
-  sg_end_pass();
   sg_commit();
 }
 
